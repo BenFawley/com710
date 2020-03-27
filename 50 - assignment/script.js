8c4a091103ff719e1973fd64b0591cab64e69c93
Ben Fawley <benfawley0@gmail.com>
2020-03-27 16:35:09 +0000

    finished login modal

--- 50 - assignment/script.js
+++ 50 - assignment/script.js
@@ -1,4 +1,27 @@
 // Get the modal
-var modal = document.getElementById("logInModal");
+var logInModal = document.getElementById("logInModal");
+
+// Get the button that opens the modal
+var logInBtn = document.getElementById("modalOpenButton");
+
+//Displays the modal when clicked
+logInBtn.onclick = function(){
+    logInModal.style.display ="block";
+}
+
+//Closes modal when x is clicked
+document.getElementById("closeButton").onclick = function(){
+    logInModal.style.display ="none";
+}
+
+
+
+
+
+
+
+
+
+    
 
 
