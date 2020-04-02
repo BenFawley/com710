460405a09ee60dcdc12f23075fded1d0be7b9ae1
Ben Fawley <benfawley0@gmail.com>
2020-04-02 11:39:00 +0100

    website

--- 50 - assignment/script.js
+++ 50 - assignment/script.js
@@ -4,6 +4,12 @@ var logInModal = document.getElementById("logInModal");
 // Get the button that opens the modal
 var logInBtn = document.getElementById("modalOpenButton");
 
+//Get sign up modal
+var signUpModal = document.getElementById("signUpModal");
+
+//Get the button that opens modal
+var signUpBtn = document.getElementById("modalSignInButton");
+
 //Displays the modal when clicked
 logInBtn.onclick = function(){
     logInModal.style.display ="block";
@@ -14,7 +20,10 @@ document.getElementById("closeButton").onclick = function(){
     logInModal.style.display ="none";
 }
 
-
+//display sign up modal
+signUpBtn.onclick = function(){
+    signUpBtn.style.display="block";
+}
 
 
 
