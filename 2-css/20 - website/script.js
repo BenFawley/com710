1689aeca35c9c92ce903f531b3de7a97bb76da1c
Ben Fawley <benfawley0@gmail.com>
2021-01-26 16:32:41 +0000

    delete unnecessary files

--- 2-css/20 - website/script.js
+++
@@ -1,8 +0,0 @@
-function toggleClass(){
-    let menu = document.querySelector(".mainMenu");
-    menu.classList.toggle("toggleCls");
-}
-
-let hamburger = document.querySelector(".hamIcon");
-
-hamburger.addEventListener("click", toggleClass);
